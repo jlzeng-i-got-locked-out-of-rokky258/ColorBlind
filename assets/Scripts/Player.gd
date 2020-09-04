27ba49cd93261b34e7c5eda6908c7f04d1d7b9d4
Richard Woods <rm-woods@hotmail.com>
2020-09-04 18:19:43 -0400

    Jumping & deceleration

--- assets/Scripts/Player.gd
+++ assets/Scripts/Player.gd
@@ -1,20 +1,23 @@
 extends KinematicBody2D
-export var maxSpeed = Vector2(300, 4000);
-var velocity: = Vector2(0, 0);
-var jumps: = 2;
-export var gravity: = 300.0;
+
+export var maxSpeed = Vector2(300, 4000)
+export var jumpSpeed = -200
+var velocity = Vector2(0, 0)
+var jumps = 2
+export var gravity = 300.0
 
 
 func _physics_process(delta:float) -> void:
-	var direction = Input.get_action_strength("move_right")- Input.get_action_strength("move_left");
-	velocity.x = maxSpeed.x * direction;
-	velocity.y += gravity * delta;
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if (direction != 0):
+		velocity.x = maxSpeed.x * direction
+	velocity.y += gravity * delta
 	if (velocity.y > maxSpeed.y):
-		velocity.y = maxSpeed.y;
-	if (Input.get_action_strength("move_jump") > 0 && jumps > 0):
-		velocity.y -= 100;
-		--jumps
+		velocity.y = maxSpeed.y
 	if (is_on_floor()):
 		jumps = 2
-	velocity = move_and_slide(velocity);
-
+	if (Input.is_action_just_pressed("move_jump") && jumps > 0):
+		velocity.y = jumpSpeed
+		jumps -= 1
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	velocity.x *= 0.65
