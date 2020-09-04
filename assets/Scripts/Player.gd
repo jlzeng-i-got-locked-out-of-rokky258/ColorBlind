0775b95565990cf482bf16aedbfb0109cfe1bf75
jlzeng-i-got-locked-out-of-rokky258 <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-09-04 18:09:52 -0400

    Code is bad
    bad

--- assets/Scripts/Player.gd
+++ assets/Scripts/Player.gd
@@ -1,13 +1,20 @@
 extends KinematicBody2D
 export var maxSpeed = Vector2(300, 4000);
 var velocity: = Vector2(0, 0);
+var jumps: = 2;
 export var gravity: = 300.0;
+
+
 func _physics_process(delta:float) -> void:
 	var direction = Input.get_action_strength("move_right")- Input.get_action_strength("move_left");
 	velocity.x = maxSpeed.x * direction;
 	velocity.y += gravity * delta;
 	if (velocity.y > maxSpeed.y):
-		velocity.y = 0;
-
+		velocity.y = maxSpeed.y;
+	if (Input.get_action_strength("move_jump") > 0 && jumps > 0):
+		velocity.y -= 100;
+		--jumps
+	if (is_on_floor()):
+		jumps = 2
 	velocity = move_and_slide(velocity);
 
