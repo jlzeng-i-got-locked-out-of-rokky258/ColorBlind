41b3fe727554af10cec30a238ee8b8e6758aae28
jlzeng-i-got-locked-out-of-rokky258 <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-09-04 18:47:46 -0400

    Added dash and sprite facing and made player worse

--- assets/Scripts/Player.gd
+++ assets/Scripts/Player.gd
@@ -1,16 +1,20 @@
 extends KinematicBody2D
 
-export var maxSpeed = Vector2(300, 4000)
-export var jumpSpeed = -200
+export var maxSpeed = Vector2(225, 4000)
+export var jumpSpeed = -170
 var velocity = Vector2(0, 0)
 var jumps = 2
-export var gravity = 300.0
+export var gravity = 800.0
 
 
 func _physics_process(delta:float) -> void:
 	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	if (direction != 0):
 		velocity.x = maxSpeed.x * direction
+		if (direction > 0):
+			get_node( "Sprite" ).set_flip_h( false )
+		if (direction < 0):
+			get_node( "Sprite" ).set_flip_h( true )
 	velocity.y += gravity * delta
 	if (velocity.y > maxSpeed.y):
 		velocity.y = maxSpeed.y
@@ -19,5 +23,10 @@ func _physics_process(delta:float) -> void:
 	if (Input.is_action_just_pressed("move_jump") && jumps > 0):
 		velocity.y = jumpSpeed
 		jumps -= 1
+	if (Input.is_action_just_pressed("move_dash")):
+		if(direction > 0):
+			velocity.x = 800
+		if(direction < 0):
+			velocity.x = -800
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	velocity.x *= 0.65
