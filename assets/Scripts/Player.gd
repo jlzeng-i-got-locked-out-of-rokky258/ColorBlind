db2806a9846e292da72d7fa11fb4eb576ecd92dc
jlzeng-i-got-locked-out-of-rokky258 <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-09-04 15:18:25 -0400

    Left and Right code

--- assets/Scripts/Player.gd
+++ assets/Scripts/Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
-export var maxSpeed = Vector2(300, 1000);
+export var maxSpeed = Vector2(300, 4000);
 var velocity: = Vector2(0, 0);
 export var gravity: = 300.0;
 func _physics_process(delta:float) -> void:
+	var direction = Input.get_action_strength("move_right")- Input.get_action_strength("move_left");
+	velocity.x = maxSpeed.x * direction;
 	velocity.y += gravity * delta;
 	if (velocity.y > maxSpeed.y):
 		velocity.y = 0;
+
 	velocity = move_and_slide(velocity);
-	
+
