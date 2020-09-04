0fad7a045589414254ee408df159e8700f623706
jlzeng-i-got-locked-out-of-rokky258 <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-09-04 14:40:17 -0400

    Added basic player physics and collision mask

--- assets/Scripts/Player.gd
+++ assets/Scripts/Player.gd
@@ -1,16 +1,10 @@
 extends KinematicBody2D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+export var maxSpeed = Vector2(300, 1000);
+var velocity: = Vector2(0, 0);
+export var gravity: = 300.0;
+func _physics_process(delta:float) -> void:
+	velocity.y += gravity * delta;
+	if (velocity.y > maxSpeed.y):
+		velocity.y = 0;
+	velocity = move_and_slide(velocity);
+	
