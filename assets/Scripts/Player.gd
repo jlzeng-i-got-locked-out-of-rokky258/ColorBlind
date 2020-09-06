0cebd8adc58ad46fede6ab883bb75aa25d3655aa
Richard Woods <rm-woods@hotmail.com>
2020-09-06 00:20:29 -0400

    Edited world and added particle effect on doublejump/walljump/dash

--- assets/Scripts/Player.gd
+++ assets/Scripts/Player.gd
@@ -40,6 +40,11 @@ var dashCooldownTimer = 0.0
 var SnapVector = Vector2(0, 6)
 export var MAX_SLOPE = 45.0 * PI / 180.0
 
+export var WALL_REMEMBER_TIME = 0.1
+var wallRememberTimer = 0.0
+var wallDir = 1
+
+
 func _ready():
 	Global.player = self
 	$Sprite.modulate = Color(1, 1, 1)
@@ -49,6 +54,7 @@ func _ready():
 func _physics_process(delta):
 	if !alive:
 		position.y -= 10 * delta
+		$ChuteSprite.visible = false
 		return
 	
 	var newMovement = Vector2(0, 0)
@@ -88,6 +94,7 @@ func _physics_process(delta):
 		desiredColor = Color(1, 1, 1)
 		SnapVector.y = 6.0
 		gravity = maxGravity
+		$ChuteSprite.visible = false
 	else:
 		# Don't start "falling" until a short period after leaving the ground
 		# This means we won't eat the jump until they have fallen a certain duration
@@ -97,11 +104,14 @@ func _physics_process(delta):
 		else:
 			if Input.is_action_pressed("move_jump") and velocity.y > 0.0 and canFloat: # Slow fall
 				newMovement.y += gravity * 0.1 * delta 
+				if velocity.y > (maxSpeed.y * 0.1):
+					velocity.y = maxSpeed.y * 0.1
 				if falling:
 					$Sprite.frame = 4
 					desiredColor = Color(1, 0.714844, 0.868561)
-					
+				$ChuteSprite.visible = true
 			else: # Normal fall
+				$ChuteSprite.visible = false
 				newMovement.y += gravity * delta
 				if falling:
 					desiredColor = Color(0.935638, 0.777344, 1)
@@ -113,18 +123,29 @@ func _physics_process(delta):
 					if jumps == maxJumps:
 						jumps -= 1
 	
-	if (Input.is_action_just_pressed("move_jump") && jumps > 0 && (!is_on_wall() or is_on_floor())):
+	if is_on_wall():
+		wallRememberTimer = WALL_REMEMBER_TIME
+		wallDir = dashDirection
+	else:
+		wallRememberTimer -= delta
+	
+	if (Input.is_action_just_pressed("move_jump") && jumps > 0 && (wallRememberTimer <= 0.0 or is_on_floor())):
 		velocity.y = jumpSpeed
 		falling = true
 		fallTimer = 0.0
+		if jumps != maxJumps:
+			$Particles2D.emitting = true
 		jumps -= 1
 		SnapVector.y = 0
-	if (Input.is_action_just_pressed("move_jump") && is_on_wall() && canWallJump && !is_on_floor()):
+	elif (canWallJump && Input.is_action_just_pressed("move_jump") && wallRememberTimer > 0.0  && !is_on_floor()):
 		velocity.y = jumpSpeed*1.2
 		falling = true
 		fallTimer = 0.0
 		SnapVector.y = 0
-		velocity.x = maxSpeed.x * -dashDirection *.35
+		velocity.x = maxSpeed.x * -wallDir *.35
+		$Particles2D.emitting = true
+	else:
+		$Particles2D.emitting = false
 	
 	if dashCooldownTimer <= 0.0 and Input.is_action_just_pressed("move_dash") and canDash:
 		if !dashing:
@@ -139,6 +160,7 @@ func _physics_process(delta):
 		if dashTimer <= 0.0:
 			dashCooldownTimer = DASH_COOLDOWN
 			dashing = false
+		$Particles2D.emitting = true
 	elif dashCooldownTimer > 0.0:
 		dashCooldownTimer -= delta
 		desiredColor.b = lerp(1.0, startColor.b, (DASH_COOLDOWN - dashCooldownTimer) / DASH_COOLDOWN)
