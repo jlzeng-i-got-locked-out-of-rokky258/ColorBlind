00e3d2d539160201f5c4367ce9cf05704349083b
Richard Woods <rm-woods@hotmail.com>
2020-09-06 17:54:26 -0400

    Added pickups for the abilities

---
+++ assets/Scripts/JumpPickup.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+onready var yStart = position.y
+var collected = false
+
+
+func _ready():
+	$Sprite.modulate = Color(0.935638, 0.777344, 1)
+
+
+func _process(delta):
+	if collected:
+		position.y -= 10 * delta
+		if position.y > (yStart + 5):
+			queue_free()
+		modulate.a -= delta * 2
+	else:
+		position.y = yStart + (sin(Global.time * 3) * 2) 
+
+
+func _on_Area2D_body_entered(body):
+	if not collected:
+		Global.player.maxJumps += 1
+		collected = true
