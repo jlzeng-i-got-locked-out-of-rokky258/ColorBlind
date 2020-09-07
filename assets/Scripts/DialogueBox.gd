3e9da127d06645feccdaadd58ce7a69507f5ddfd
Richard Woods <rm-woods@hotmail.com>
2020-09-06 23:19:35 -0400

    Text boxes

--- assets/Scripts/DialogueBox.gd
+++ assets/Scripts/DialogueBox.gd
@@ -10,13 +10,16 @@ var finished = false;
 func _ready():
 	load_dialog()
 
+
 func _process(_delta):
-	if Input.is_action_just_pressed("move_jump"):
+	if Input.is_action_just_pressed("ui_accept"):
 		load_dialog()
+
 
 func load_dialog():
 	if (dialog_index < dialog.size()):
 		$RichTextLabel.bbcode_text = dialog[dialog_index].to_upper()
 	else:
-		queue_free()
+		finished = true
+		visible = false
 	dialog_index += 1
